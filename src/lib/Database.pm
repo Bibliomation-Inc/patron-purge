32db70ddd45ae76fe0b4ac234cb7419f1a39a594
IanSkelskey <ianskelskey@gmail.com>
2026-01-30 08:52:39 -0500

    Add run_sql and run_sql_file functions to Database module

--- src/lib/Database.pm
+++ src/lib/Database.pm
@@ -5,7 +5,13 @@ use warnings;
 use DBI;
 use Exporter 'import';
 
-our @EXPORT_OK = qw(setup_database_connection create_table_if_not_exists create_schema_if_not_exists);
+our @EXPORT_OK = qw(
+    setup_database_connection 
+    create_table_if_not_exists 
+    create_schema_if_not_exists
+    run_sql
+    run_sql_file
+);
 
 my $database_handle = undef;
 
@@ -25,7 +31,61 @@ sub setup_database_connection {
 }
 
 # ----------------------------------------------
+# run_sql - executes SQL text with optional bind parameters
+# Arguments:
+#   $sql    - SQL statement to execute
+#   @params - optional bind parameters for placeholders
+# Returns: arrayref of hashrefs for SELECT, rows affected for other statements
+# Throws: on missing database connection, DBI errors
+# ----------------------------------------------
+
+sub run_sql {
+    my ($sql, @params) = @_;
+    
+    die "No database connection established\n" unless defined $database_handle;
+    die "No SQL provided\n" unless defined $sql && $sql =~ /\S/;
+    
+    my $sth = $database_handle->prepare($sql);
+    $sth->execute(@params);
+    
+    # If it's a SELECT, return results as arrayref of hashrefs
+    if ($sql =~ /^\s*SELECT/i) {
+        my $results = $sth->fetchall_arrayref({});
+        $sth->finish();
+        return $results;
+    }
+    
+    # For non-SELECT, return number of rows affected
+    my $rows = $sth->rows;
+    $sth->finish();
+    return $rows;
+}
+
+# ----------------------------------------------
+# run_sql_file - reads SQL from a file and executes it with optional bind parameters
+# Arguments:
+#   $file_path - path to the SQL file
+#   @params    - optional bind parameters for placeholders
+# Returns: arrayref of hashrefs for SELECT, rows affected for other statements
+# Throws: on file read failure, missing database connection, DBI errors
+# ----------------------------------------------
+
+sub run_sql_file {
+    my ($file_path, @params) = @_;
+    
+    die "No SQL file path provided\n" unless defined $file_path;
+    die "SQL file not found: $file_path\n" unless -e $file_path;
+    
+    open my $fh, '<', $file_path or die "Cannot open SQL file '$file_path': $!\n";
+    my $sql = do { local $/; <$fh> };
+    close $fh;
+    
+    return run_sql($sql, @params);
+}
+
+# ----------------------------------------------
 # create_table_if_not_exists - creates a table if it does not already exist given a table name and schema
+# Throws: on missing database connection, DBI errors
 # ----------------------------------------------
 
 sub create_table_if_not_exists {
@@ -42,6 +102,7 @@ sub create_table_if_not_exists {
 
 # ----------------------------------------------
 # create_schema_if_not_exists - creates a schema if it does not already exist given a schema name
+# Throws: on missing database connection, DBI errors
 # ----------------------------------------------
 
 sub create_schema_if_not_exists {
